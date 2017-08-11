952ff4306c4d2593e1b5383bdc2be535a55d8b0f
utp <utp@utp>
2017-08-11 07:37:51 -0500

    Make file

---
+++ Server music 1.0/play.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+using namespace sf;
+
+int main(int argc, chat **argv) {
+  if (argc != 2) {
+    cerr << "Must be called: " << argv[0] << "file.ogg\n";
+    return 1;
+  }
+  cout << "Simple player! \n";
+
+}
