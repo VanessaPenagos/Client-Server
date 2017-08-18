9aa85b29c746bf7a483cafda10df0914b13636ed
Vanessa Penagos <vanessa@localhost.localdomain>
2017-08-18 08:57:31 -0500

    No funcional

--- Music server/client.cpp
+++ Music server/client.cpp
@@ -1,70 +1,75 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <zmqpp/zmqpp.hpp>
-#include <SFML/Audio.hpp>
 
 using namespace std;
 using namespace zmqpp;
 using namespace sf;
 
-void messageToFile(const message& msg, const string& fileName) {
-	const void *data;
-	msg.get(&data, 1);
-	size_t size = msg.size(1);
+void messageToFile(const message &msg, const string &fileName) {
+  const void *data;
+  msg.get(&data, 1);
+  size_t size = msg.size(1);
 
-	ofstream ofs(fileName, ios::binary);
-	ofs.write((char*)data, size);
+  ofstream ofs(fileName, ios::binary);
+  ofs.write((char *)data, size);
 }
 
-int main(int argc, char** argv) {
-	cout << "This is the client\n";
+int main() {
+  cout << "This is the client\n";
 
-	context ctx;
-	socket s(ctx, socket_type::req);
-	cout << "Connecting to tcp port 5555\n";
-	s.connect("tcp://localhost:5555");
+  context ctx;
+  socket s(ctx, socket_type::req);
+  cout << "Connecting to tcp port 5555\n";
+  s.connect("tcp://localhost:5555");
+  Music music;
 
-	Music music;
+  while (true) {
+    cout << "Operacion? " << endl;
+    string operation;
+    cin >> operation;
 
-	cout << "Sending  some work!\n";
+    message m;
+    m << operation;
 
-	message m;
-	string operation(argv[1]);
+    if (operation == "play") {
+      string file;
+      cin >> file;
+      m << file;
+    } else if (operation == "exit") {
+      return 0;
+    }
 
-	m << operation;
+    s.send(m);
 
-	if(argc == 3) {
-		string file(argv[2]);
-		m << file;
-	}
+    message answer;
+    s.receive(answer);
 
-	s.send(m);
+    string result;
+    answer >> result;
+    if (result == "list") {
+			cout << "Hola" << endl;
+     	size_t numSongs;
+      answer >> numSongs;
+      cout << "Available songs: " << numSongs << endl;
+      for (size_t i = 0; i < numSongs; i++) {
+        string s;
+        answer >> s;
+        cout << s << endl;
+      }
 
-	message answer;
-	s.receive(answer);
-
-	string result;
-	answer >> result;
-	if (result == "list") {
-		size_t numSongs;
-		answer >> numSongs;
-		cout << "Available songs: " << numSongs << endl;
-		for(int i = 0; i < numSongs; i++) {
-			string s;
-			answer >> s;
-			cout << s << endl;
-		}
-
-	} else if (result == "file"){
-		messageToFile(answer,"song.ogg");
-		music.openFromFile("song.ogg");
-		music.play();
-		int x;
-		cin >> x;
-	} else {
-		cout << "Don't know what to do!!!" << endl;
-	}
-
-	return 0;
+     } else if (result == "file") {
+      messageToFile(answer, "song.ogg");
+      music.openFromFile("song.ogg");
+      music.play();
+      while (music.getStatus() == SoundSource::Playing) {
+        cout << "suena!" << endl;
+      }
+    } else {
+      cout << "Don't know what to do!!!" << endl;
+    }
+   }
+  return 0;
 }
